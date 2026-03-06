c0a2863d4d39e5a548daecb87c7724ca7d440fda
Cristian <criscubillos@hotmail.com>
2026-03-06 00:51:24 -0300

    Replace GTM with Google Ads gtag integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,12 +74,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className={`${orbitron.variable} ${shareTechMono.variable}`}>
       <head>
         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17991893351"
+        />
+        <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TD8TWQ6F');`,
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'AW-17991893351');`,
           }}
         />
         <script
@@ -88,17 +92,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           crossOrigin="anonymous"
         ></script>
       </head>
-      <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TD8TWQ6F"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
