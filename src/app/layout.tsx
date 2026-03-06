98b836988bd0446cfd906402709cb13e712af6c9
Cristian <criscubillos@hotmail.com>
2026-03-06 08:47:07 -0300

    Align Google Tag Manager layout with Next docs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,17 +81,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           crossOrigin="anonymous"
         ></script>
       </head>
-      <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TD8TWQ6F"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
